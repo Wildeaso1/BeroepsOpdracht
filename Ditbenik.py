654c8ddb7416dd7b1ad5b341da5c6ff1d7d53be7
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2020-09-18 13:51:41 +0200

    Add files via upload

---
+++ Ditbenik.py
@@ -0,0 +1,61 @@
+print("Hello You!, Ik ben William!")
+print("Wie ben jij?")
+
+naam = input()
+print("Hallo!,", naam)
+print("Om mij beter te leren kennen heb ik wat multiple choice vragen gemaakt! Voer alsjeblieft A,B of C in!")
+
+print("Waar woon ik")
+
+print("A: Zaandam")
+print("B: Amsterdam")
+print("C: Lelystad")
+
+answer = input()
+
+if answer == 'A':
+	print("Correct! Ik woon nu al 5 jaar in zaandam!")
+
+if answer == "B":
+	print("Fout. Ik zit wel in Amsterdam op school.")
+
+if answer == "C":
+	print("Fout. Ik heb wel in lelystad gewoond.")
+
+print("Mijn droom is?")
+
+print("A: Bij een groot game bedrijf te werken.")
+print("B: Mijn eigen bedrijf te maken en games te maken.")
+print("C: Mijn eigen websites te bouwen.")
+
+answer = input()
+
+if answer == "A":
+	print("Fout!")
+
+if answer == "B":
+	print("Correct. Ik wil inderdaad me eigen games maken.")
+
+if answer == "C":
+	print("Fout!")
+
+print("In mijn vrije tijd doe ik?")
+
+print("A: Ik ga veel naar buiten")
+print("B: Ik game heel veel en teken veel") 
+print("C: Ik lees heel veel boeken")
+
+answer = input()
+
+if answer == "A":
+	print("Fout, Ik ga soms naar buiten")
+
+if answer == "B":
+	print("Correct. Ik game en heel veel")
+
+if answer == "C":
+	print("Fout!")
+
+print("Nu heb je me iets beter leren kennen!")
+
+
